4504a942d9f737f272a9b4338c362aba0f9a373b
cissouvs <illiencyriac13@gmail.com>
2026-07-01 10:58:14 +0200

    Premier commit

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UrbanCanvas
+//
+//  Created by apprenant92 on 01/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UrbanCanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  UrbanCanvasApp.swift
+//  UrbanCanvas
+//
+//  Created by apprenant92 on 01/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct UrbanCanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
